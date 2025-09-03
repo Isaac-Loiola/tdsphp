6c8523bde6a15b8e1a4c9d8f5d52a953fb34f3ed
Isaac Oliveira Loiol <isaackopi@icloud.com>
2025-09-03 11:52:03 -0300

    Adicionando php no html!

--- index.php
+++ index.php
@@ -8,4 +8,40 @@
 
 # Comentário de linha
 
-echo "Olá mundo!";
+/*
+    PHP é um linguagem de programação volrada principalmente para dev WEB.
+    Server-side.
+    Funciona em conjunto com HTML, CSS, JS e integra com Banco de dados.
+    Nasceu em 1994.
+    Estrutura pra funcionar ( Server Linux, WAMPP, XAMPP, Laragon) - Ambiente de desenvolvimento.
+    Orientado a expressão.
+
+*/
+
+echo "Olá mundo!"; 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $nome = "Isaac";
+        $idade = 17;
+        $email = "isaacoliveirakopi@gmail.com"
+    ?>
+    <h2>Nome: 
+        <?php
+            #concatenação de strings
+            echo "Meu nome é: ".$nome.",\n tenho ".$idade." anos,\n e meu email é ".$email."";  
+        ?>
+    </h2>
+    <?php
+        
+    ?>
+</body>
+</html>
