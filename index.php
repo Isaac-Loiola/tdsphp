b427f8576f0874a8c1f466269db929087a347c43
Isaac Oliveira Loiol <isaackopi@icloud.com>
2025-09-04 09:49:51 -0300

    utilizando operadores aritméticos

--- index.php
+++ index.php
@@ -18,30 +18,55 @@
 
 */
 
-echo "Olá mundo!"; 
+// echo "Olá mundo!"; 
+
+// Declaração de variáveis no PHP
+$A123;
+$x = 1.6; // = (atribuição) / == (comparação) / === (comparação absoluta tipo e valor)
+
+$X = 2; // PHP é case sensitive (faz diferenciação entre maiúsculo e minúsculo)
+
+$escola = "Senac";
+$numero = 8266;
+$valorOnibus = 5;
+$portaoAberto = true;
+
+echo "Estudo no $escola em $numero da Av.Itaquera,
+e pago $valorOnibus na passagem de onibus.
+Aberto = $portaoAberto";
+
+
+// variáveis globais no PHP
+// echo $_SERVER;
+
+// print_r($_SERVER);
+
+
+// constantes no PHP
+echo "<br>";
+
+define("TESTE", "conteúdo de um constante");
+define("DB_NAME", "ComercialDB01");
+define("DB_USER", "root");
+define("DB_PASSWD", "123 ");
+define("DB_SERVER", "10.91.47.77");
+
+echo DB_NAME." - ".DB_USER;
+
+
+// Operadores no PHP
+echo "<br>";
+$a = 2;
+$b = 10;
+
+echo -$a."<br>";
+echo -$b."<br>";
+echo "$a + $b = ".$a+$b."<br>"; # adição
+echo "$a - $b = ".$a-$b."<br>"; # subtração
+echo "$a / $b = ".$a/$b."<br>"; # divisão
+echo "$a % $b = ".$a%$b."<br>"; # resto da divisão (mod)
+echo "$a ** $b = ".$a**$b."<br>"; # exponenciação (Elevado)
+
+
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        $nome = "Isaac";
-        $idade = 17;
-        $email = "isaacoliveirakopi@gmail.com"
-    ?>
-    <h2>Nome: 
-        <?php
-            #concatenação de strings
-            echo "Meu nome é: ".$nome.",\n tenho ".$idade." anos,\n e meu email é ".$email."";  
-        ?>
-    </h2>
-    <?php
-        
-    ?>
-</body>
-</html>
