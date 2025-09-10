03646c2b89d3c367cbcfce704dc0a1c44bfae010
Isaac Oliveira Loiol <isaackopi@icloud.com>
2025-09-10 11:06:35 -0300

    Cadastrando um usuario na base de dados!

--- conecta.php
+++ conecta.php
@@ -1,14 +1,26 @@
 <?php 
 
 try{
-    $pdo = new PDO("mysql:host=10.91.47.77;dbname-modelophp_db", "root", "123");
+    $pdo = new PDO("mysql:host=10.91.47.77;dbname=modelophp_db", "root", "123");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "A conexão rolou!";
 
     // Inserindo usuario
+    $stmt = $pdo->prepare("insert into usuarios (nome, email, datacad)
+    values (:nome, :email, default)");
+
+    $stmt->execute([
+        ":nome" => "Isaac Loiola",
+        ":email" => "isaackopi@icloud.com"
+    ]);
 }
 catch(PDOException $e){
-    echo "Erro: ".$e->getMessage();
+    if(strpos($e->getMessage(), "Duplicate") !== false){
+        echo "Email já cadastrado na base de dados. <br>";
+    }
+    else{
+        echo $e->getMessage();
+    }
 }
 
 ?>
